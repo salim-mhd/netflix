94004980265ed3bd26f02a123b9143373980e2ae
muahmmed salim <salimmhd1280@gmail.com>
2022-11-02 19:26:24 +0530

    add All Catagory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
-import { orginals,action,animation } from "./urls";
+import { orginals, action, animation, comedy, thriller, romance, documentary, adventure, crime, drama, family, fantasy, history, horror, music, mystery, scienceFiction, war } from "./urls";
 import './App.css'
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
@@ -8,11 +8,26 @@ import RowPost from "./Components/RowPost/RowPost";
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Banner/>
+      <NavBar />
+      <Banner />
       <RowPost url={orginals} title='Netflix Orginals' />
       <RowPost url={action} title='Action' isSmall />
       <RowPost url={animation} title='Animation' isSmall />
+      <RowPost url={comedy} title='Comedy' isSmall />
+      <RowPost url={thriller} title='Thriller' isSmall />
+      <RowPost url={romance} title='Romance' isSmall />
+      <RowPost url={documentary} title='Documentary' isSmall />
+      <RowPost url={adventure} title='Adventure' isSmall />
+      <RowPost url={crime} title='Crime' isSmall />
+      <RowPost url={drama} title='Drama' isSmall />
+      <RowPost url={family} title='Family' isSmall />
+      <RowPost url={fantasy} title='Fantasy' isSmall />
+      <RowPost url={history} title='History' isSmall />
+      <RowPost url={horror} title='Horror' isSmall />
+      <RowPost url={music} title='Music' isSmall />
+      <RowPost url={mystery} title='Mystery' isSmall />
+      <RowPost url={scienceFiction} title='ScienceFiction' isSmall />
+      <RowPost url={war} title='War' isSmall />
     </div>
   );
 }
--- src/urls.js
+++ src/urls.js
@@ -1,4 +1,20 @@
 import { API_KEY } from "./constants/constants";
-export const orginals =`discover/tv?api_key=${API_KEY}&with_networks=213`;
-export const action=`discover/movie?api_key=${API_KEY}&with_genres=28`;
-export const animation=`discover/movie?api_key=${API_KEY}&with_genres=16`;
+
+export const orginals = `discover/tv?api_key=${API_KEY}&with_networks=213`;
+export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`;
+export const animation = `discover/movie?api_key=${API_KEY}&with_genres=16`;
+export const comedy = `discover/movie?api_key=${API_KEY}&with_genres=35`;
+export const thriller = `discover/movie?api_key=${API_KEY}&with_genres=53`;
+export const romance = `discover/movie?api_key=${API_KEY}&with_genres=10749`;
+export const documentary = `discover/movie?api_key=${API_KEY}&with_genres=99`;
+export const adventure = `discover/movie?api_key=${API_KEY}&with_genres=12`;
+export const crime = `discover/movie?api_key=${API_KEY}&with_genres=80`;
+export const drama = `discover/movie?api_key=${API_KEY}&with_genres=18`;
+export const family = `discover/movie?api_key=${API_KEY}&with_genres=10751`;
+export const fantasy = `discover/movie?api_key=${API_KEY}&with_genres=14`;
+export const history = `discover/movie?api_key=${API_KEY}&with_genres=36`;
+export const horror = `discover/movie?api_key=${API_KEY}&with_genres=27`;
+export const music = `discover/movie?api_key=${API_KEY}&with_genres=10402`;
+export const mystery = `discover/movie?api_key=${API_KEY}&with_genres=9648`;
+export const scienceFiction = `discover/movie?api_key=${API_KEY}&with_genres=878`;
+export const war = `discover/movie?api_key=${API_KEY}&with_genres=10752`;
